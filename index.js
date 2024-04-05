c6830ca2124d8a03a1c63bad99604abb299d64db
ummetarin <ummehomairatarin@gmail.com>
2024-04-05 09:56:42 +0600

    Complete cart data work from backend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port=process.env.PORT||5000
 require('dotenv').config()
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fdvzwaw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,15 +23,48 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-     const dressdata=client.db('Ecomdata').collection('Alldress')
+     const dressdata=client.db('Ecomdata').collection('Alldress');
+     const carddata=client.db('Ecomdata').collection('carddata');
+     const reviewdata=client.db('Ecomdata').collection('revdata');
 
     //  dressdata
-
-    app.get('/dress',async(req,res)=>{
-        const result=await dressdata.find().toArray();
-        res.send(result);
+    app.get('/dress',async (req,res)=>{
+      const result= await dressdata.find().toArray();
+      res.send(result);
     })
 
+     app.get('/dress/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={ _id : new ObjectId(id) }
+      const user=await dressdata.findOne(query);
+      res.send(user);
+     })
+    
+    //  review
+
+   
+
+    // card
+
+    app.get('/allcarddata',async(req,res)=>{
+      const result=await carddata.find().toArray();
+      res.send(result)
+    })
+
+    app.get('/addcard',async (req,res)=>{
+      const email=req.query.email
+      const query={ email : email }
+      const result= await carddata.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/addcard',async (req,res)=>{
+      const cartitem=req.body;
+      const result=await carddata.insertOne(cartitem)
+      res.send(result);
+
+    })
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
